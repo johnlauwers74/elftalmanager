05889fa98b6c9384b2ef54bd5e5bc1490225c0fa
John Lauwers <161937095+johnlauwers74@users.noreply.github.com>
2026-02-09 16:20:12 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://riqydupophnmumcibolp.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_DBw25_Gs3NEuQDs4PCXU3Q_OONl2AiL';
+const SUPABASE_URL = 'https://hdduodfdzmoqdfudykpv.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkZHVvZGZkem1vcWRmdWR5a3B2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyOTAyMzQsImV4cCI6MjA4NTg2NjIzNH0.p6ceLnMfdnDB3orkx1DLJ1YmZEF47Z1JQ2HytbllfK0';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
