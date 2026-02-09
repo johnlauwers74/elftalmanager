f733e93d7c86473983111b1601dae520de8e7226
John Lauwers <161937095+johnlauwers74@users.noreply.github.com>
2026-02-09 16:23:33 +0100

    Update supabase.ts

--- lib/supabase.ts
+++ lib/supabase.ts
@@ -1,7 +1,7 @@
 
 import { createClient } from '@supabase/supabase-js';
 
-const SUPABASE_URL = 'https://hdduodfdzmoqdfudykpv.supabase.co';
-const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImhkZHVvZGZkem1vcWRmdWR5a3B2Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzAyOTAyMzQsImV4cCI6MjA4NTg2NjIzNH0.p6ceLnMfdnDB3orkx1DLJ1YmZEF47Z1JQ2HytbllfK0';
+const SUPABASE_URL = 'https://riqydupophnmumcibolp.supabase.co';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InJpcXlkdXBvcGhubXVtY2lib2xwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzA0Njk0ODcsImV4cCI6MjA4NjA0NTQ4N30.I-ljTbYGlLH-excKEIxUrNbD0s7e52nwuGUlqsbVUTo';
 
 export const supabase = createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
